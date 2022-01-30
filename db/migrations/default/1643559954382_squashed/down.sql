7e0a874dd86901e3ca2754390f8cabe5395d14c0
Zayd Selmani <zayd448@gmail.com>
2022-01-30 17:32:02 +0100

    first migrations

---
+++ db/migrations/default/1643559954382_squashed/down.sql
@@ -0,0 +1,12 @@
+
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- drop schema "topology" cascade;
+
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- drop schema "tiger_data" cascade;
+
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- drop schema "tiger" cascade;
---
+++ db/migrations/default/1643559954382_squashed/up.sql
@@ -0,0 +1,8 @@
+
+SET check_function_bodies = false;
+
+drop schema "tiger" cascade;
+
+drop schema "tiger_data" cascade;
+
+drop schema "topology" cascade;
